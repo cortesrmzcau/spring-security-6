3b227a55d11f11b42378baa68e213ecab3ef8dbc
Cesar <cesar.a.cortes.94@gmail.com>
2023-12-24 19:47:44 -0600

    Sección 2: User Details Service

---
+++ README.md
@@ -0,0 +1,34 @@
+## Hello World! 👋
+
+🤩 [Curso: Spring security 6 Desde las bases hasta OAUTH2 y JWT](https://www.udemy.com/course/spring-security-6-desde-las-bases-hasta-oauth2-y-jwt/>)
+
+-------------
+
+## ⚙ Tecnologías aplicadas
+
+[![Java](https://img.shields.io/badge/Java-007396?style=for-the-badge&logo=java&logoColor=white&labelColor=101010)]()
+[![SpringBoot](https://img.shields.io/badge/SpringBoot-3DDC84?style=for-the-badge&logo=spring&logoColor=white&labelColor=101010)]()
+[![Postgresql](https://img.shields.io/badge/Postgresql-007396?style=for-the-badge&logo=postgresql&logoColor=white&labelColor=101010)]()
+[![Docker](https://img.shields.io/badge/Docker-1d63ed?style=for-the-badge&logo=docker&logoColor=white&labelColor=101010)]()
+
+-------------
+
+## 🚀 Resumen
+
+### Sección 2
+
+* UserDetailService se encarga de cargar cualquier usuario de una base de datos mediante el username.
+    * Sirve para aplicaciones pequeñas.
+* InMemoryUserDetailsManager sirve para cambiar la contraseña del usuario ADMIN y USER de la contraseña que viene por default.
+    - Sirve para pruebas pequeñas cuando se este implementando seguridad.
+
+-------------
+## Encuéntrame
+
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-cesar_augusto_cortes_ramirez-0077B5?style=for-the-badge&logo=linkedin&logoColor=white&labelColor=101010)](https://www.linkedin.com/in/cortesrmzcau)
+
+[![Platzi](https://img.shields.io/badge/Platzi-cesar_augusto_cortes_ramirez-0aeb8b?style=for-the-badge&logo=platzi&logoColor=white&labelColor=101010)](https://platzi.com/p/cortesrmzca)
+
+[![LinkedIn](https://img.shields.io/badge/Udemy-cesar_augusto_cortes_ramirez-a435f0?style=for-the-badge&logo=Udemy&logoColor=white&labelColor=101010)](https://www.udemy.com/user/cesar-augusto-cortes-ramirez)
+
+[![Behance](https://img.shields.io/badge/Behance-cesar_augusto_cortes_ramirez-0056ff?style=for-the-badge&logo=Behance&logoColor=white&labelColor=101010)](https://www.behance.net/cortesrmzca)
--- src/main/java/org/cortesrmzcau/security/SecurityConfiguration.java
+++ src/main/java/org/cortesrmzcau/security/SecurityConfiguration.java
@@ -2,9 +2,20 @@ package org.cortesrmzcau.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
@@ -14,7 +25,7 @@ public class SecurityConfiguration {
         try {
             httpSecurity.authorizeHttpRequests(auth ->
                     // anyRequest.permiteAll() es para permitir que se acceda a cualquier endpoint
-                    auth.requestMatchers("/loans", "/about_us", "/welcome").authenticated()
+                    auth.requestMatchers("/loans", "/about_us", "/welcome", "/cards").authenticated()
                             .anyRequest().permitAll())
                             .formLogin(Customizer.withDefaults())
                             .httpBasic(Customizer.withDefaults());
@@ -24,4 +35,31 @@ public class SecurityConfiguration {
             throw new IllegalStateException(exception);
         }
     }
+
+    /*@Bean
+    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails admin = User.withUsername("admin")
+                .password("123")
+                .authorities("ADMIN")
+                .build();
+
+        UserDetails user = User.withUsername("user")
+                .password("123")
+                .authorities("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }*/
+
+    // Si se usa CustomerUserDetails esta parte se comenta
+    /*@Bean
+    UserDetailsService userDetailsService (DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }*/
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
